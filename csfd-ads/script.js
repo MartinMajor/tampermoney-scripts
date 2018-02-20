9b9cd6733bd275560a6ad8b771e56a6cc3afc6f4
Martin Major <martin.major@email.cz>
2018-02-20 23:08:23 +0100

    csfd-ads version 1.1

--- csfd-ads/script.js
+++ csfd-ads/script.js
@@ -4,11 +4,11 @@
 // @grant        none
 // @icon         https://cdn.rawgit.com/MartinMajor/tampermoney-scripts/master/csfd-ads/icon.png
 // @downloadURL  https://cdn.rawgit.com/MartinMajor/tampermoney-scripts/master/csfd-ads/script.js
-// @version      1.0
+// @version      1.1
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    $("div[id$='ad-wrapper']").remove();
+    $("div[id$='ad-wrapper'], div[id$='-adwrapper'], .ad-pmg").remove();
 })();
