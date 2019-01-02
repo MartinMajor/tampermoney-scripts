f65eda268e61c580481bf994db22f4c5f1f6de3d
Martin Major <martin.major@email.cz>
2019-01-02 23:40:26 +0100

    zive-ads version 1.1

--- zive-ads/script.js
+++ zive-ads/script.js
@@ -5,10 +5,13 @@
 // @grant        none
 // @icon         https://cdn.rawgit.com/MartinMajor/tampermoney-scripts/master/zive-ads/icon.png
 // @downloadURL  https://cdn.rawgit.com/MartinMajor/tampermoney-scripts/master/zive-ads/script.js
-// @version      1.0
+// @version      1.1
 // ==/UserScript==
 (function() {
     'use strict';
 
-    document.getElementsByClassName('branding-wrapper')[0].previousSibling.previousSibling.remove();
+    var xpath = "//script[contains(text(),'_SAS.ad')]";
+    var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+    matchingElement.parentElement.parentElement.remove();
 })();
